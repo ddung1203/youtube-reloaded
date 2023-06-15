935fb34ecdd1eaae54e45e9e00936aded5ef607f
Joongseok Jeon <jeonjungseok1203@gmail.com>
2023-06-15 22:26:07 +0900

    [ADD] Router, Controller

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
-const PORT = 3000;
+const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const handleListening = () => console.log(`Server listening on http://localhost:${PORT}`)
+
+app.use("/", globalRouter)
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+const handleListening = () =>
+  console.log(`Server listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
