95efc5a062274e30348110b522ddccda313db338
Joongseok Jeon <jeonjungseok1203@gmail.com>
2023-06-15 23:52:58 +0900

    [ADD] Router, Controller

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,15 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+export const see = (req, res) => {
+  console.log(req.params);
+  return res.send("See Video");
+};
+export const edit = (req, res) => {
+  console.log(req.params);
+  res.send("Edit Video");
+};
+export const search = (req, res) => res.send("Search Video");
+export const upload = (req, res) => res.send("Upload Video");
+export const deleteVideio = (req, res) => {
+  console.log(req.params);
+  res.send("Delete Video");
+};
