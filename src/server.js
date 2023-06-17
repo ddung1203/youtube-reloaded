b9185ef1b6ddc009f006b77cd388dca9c4495a91
Joongseok Jeon <jeonjungseok1203@gmail.com>
2023-06-17 16:09:33 +0900

    [ADD] Mongo

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
+const PORT = 3000;
 
 const app = express();
 const logger = morgan("dev");
@@ -18,6 +19,6 @@ app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
 const handleListening = () =>
-  console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`-> Server listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
