f6c37848497dec0910ad1df5b8c5505a453fee1e
Joongseok Jeon <jeonjungseok1203@gmail.com>
2023-06-19 11:22:12 +0900

    [ADD] User Join, Login

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import session from "express-session";
+import globalRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
 
 const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
-app.set("views", process.cwd() + "/src/views")
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true })); //form to javascript: req.body
-app.use("/", globalRouter)
+app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
